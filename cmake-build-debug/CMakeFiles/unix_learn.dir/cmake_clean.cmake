558ed16faa99b22515dad70c490c38ae3e9edab7
笑云天（潘龙） <lpan@yoozoo.com>
2018-06-15 12:26:21 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/unix_learn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unix_learn.dir/s1/demo.c.o"
+  "CMakeFiles/unix_learn.dir/s1/ls.c.o"
+  "unix_learn.pdb"
+  "unix_learn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unix_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
